ef8f306c1ceb2abd598d5444b67756e92b69db24
Yuttana Suttasupa <yuttana.su@gmail.com>
2013-02-04 16:57:13 +0700

    pick sum

--- scenario1/main.cpp
+++ scenario1/main.cpp
@@ -1,9 +1,15 @@
 
 #include <stdio.h>
 
+int sum(int a, int b)
+{
+	return a + b;
+}
+
 int main()
 {
 	printf("Hello World\n");
 	printf("Hello Nation\n");
+	printf("Hello World %d\n", sum(5, 10));
 	return 0;
 }

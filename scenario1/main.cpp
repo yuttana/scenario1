9e0ac27234dea9eff6a414ec7f966c9780c78508
Yuttana Suttasupa <yuttana.su@gmail.com>
2013-02-04 16:58:17 +0700

    add minus

--- scenario1/main.cpp
+++ scenario1/main.cpp
@@ -6,8 +6,14 @@ int sum(int a, int b)
 	return a + b;
 }
 
+int minus(int a, int b)
+{
+	return a - b;
+}
+
 int main()
 {
 	printf("Hello World %d\n", sum(5, 10));
+	printf("Hello World %d\n", minus(5, 10));
 	return 0;
 }
